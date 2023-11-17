46547f14d38b38fe77e3c93fea3895c7f4774dd5
Vedellan <61517039+Vedellan@users.noreply.github.com>
2023-11-17 22:41:02 +0900

    플레이어 이동 생성
    - 플레이어 이동 생성

--- Assets/02. Scripts/Player.cs
+++ Assets/02. Scripts/Player.cs
@@ -1,12 +1,24 @@
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class Player : MonoBehaviour
 {
     public float speed = 10f;
+    public float jumpSpeed = 10f;
+    CharacterController characterController;
+
+    float mouseX = 0;
+    float mouseY = 0;
+
+    public float gravity = -20f;
+    public float yVelocity = 0;
 
     private void Update()
     {
+        characterController = GetComponent<CharacterController>();
+
         Move();
+        Rotate();
     }
 
     void Move()
@@ -14,6 +26,35 @@ public class Player : MonoBehaviour
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
 
-        transform.Translate(speed * Time.deltaTime * Vector3.forward);
+        Vector3 moveDirection = new(horizontal, 0, vertical);
+
+        if(characterController.isGrounded)
+        {
+            yVelocity = 0;
+
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                yVelocity = jumpSpeed;
+            }
+        }
+
+        yVelocity += (gravity * Time.deltaTime);
+
+        moveDirection.y = yVelocity;
+
+        characterController.Move(speed * Time.deltaTime * transform.TransformDirection(moveDirection));
+    }
+
+    void Rotate()
+    {
+        // transform.Rotate(0f, Input.GetAxis("Mouse X") * speed, 0f, Space.World);
+        // transform.Rotate(Mathf.Clamp(-Input.GetAxis("Mouse Y") * speed, -55.0f, 55.0f), 0f, 0f);
+        // https://makerejoicegames.tistory.com/131
+        mouseX += Input.GetAxis("Mouse X") * speed;
+
+        mouseY += Input.GetAxis("Mouse Y") * speed;
+        mouseY = Mathf.Clamp(mouseY, -55.0f, 55.0f);
+
+        transform.localEulerAngles = new(-mouseY, mouseX, 0);
     }
 }
