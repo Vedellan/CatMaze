f59c0458da2cfe45a0f5e5421150538a220dee94
Vedellan <61517039+Vedellan@users.noreply.github.com>
2023-11-22 14:12:15 +0900

    이동 키 섞기
    - 시작할 때마다 새로운 이동키 4개가 랜덤으로 생성되는 로직 개발

--- Assets/02. Scripts/Player.cs
+++ Assets/02. Scripts/Player.cs
@@ -1,5 +1,7 @@
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UIElements;
 
 public class Player : MonoBehaviour
 {
@@ -13,22 +15,63 @@ public class Player : MonoBehaviour
     public float gravity = -20f;
     public float yVelocity = 0;
 
-    private void Update()
+    // 상/하/좌/우 순서, A: 97, Z: 122, 26개
+    public int[] moveKeys;
+
+    #region 값 할당
+    void AssignObjects()
     {
         characterController = GetComponent<CharacterController>();
+        moveKeys = new int[4] { 'w', 'a', 's', 'd' };
+    }
+    #endregion 값 할당
 
+    private void Awake()
+    {
+        AssignObjects();
+        MakeRandomMoveKeys();
+    }
+
+    private void Update()
+    {
         Move();
         Rotate();
     }
 
     void Move()
     {
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
+        // https://acredev.tistory.com/18
+        float horizontal = 0;
+        float vertical = 0;
+
+        // 위쪽 이동
+        if (Input.GetKey((KeyCode)moveKeys[0]))
+        {
+            Debug.Log((KeyCode)moveKeys[0]);
+            horizontal += 1;
+        }
+        // 아래쪽 이동
+        if (Input.GetKey((KeyCode)moveKeys[1]))
+        {
+            Debug.Log((KeyCode)moveKeys[1]);
+            horizontal -= 1;
+        }
+        // 왼쪽 이동
+        if (Input.GetKey((KeyCode)moveKeys[2]))
+        {
+            Debug.Log((KeyCode)moveKeys[2]);
+            vertical -= 1;
+        }
+        // 오른쪽 이동
+        if (Input.GetKey((KeyCode)moveKeys[3]))
+        {
+            Debug.Log((KeyCode)moveKeys[3]);
+            vertical += 1;
+        }
 
         Vector3 moveDirection = new(horizontal, 0, vertical);
 
-        if(characterController.isGrounded)
+        if (characterController.isGrounded)
         {
             yVelocity = 0;
 
@@ -56,5 +99,25 @@ public class Player : MonoBehaviour
         mouseY = Mathf.Clamp(mouseY, -55.0f, 55.0f);
 
         transform.localEulerAngles = new(-mouseY, mouseX, 0);
+    }
+
+    void MakeRandomMoveKeys()
+    {
+        for(int i = 0; i < moveKeys.Length; i++)
+        {
+            moveKeys[i] = Random.Range(97, 122);
+
+            for(int j = 0; j < i; j++)
+            {
+                // 중복된 키가 할당된 경우
+                if (moveKeys[i] == moveKeys[j])
+                {
+                    i--;
+                    break;
+                }
+            }
+
+            // i가 1 감소했기에 다시 실행된다.
+        }
     }
 }
